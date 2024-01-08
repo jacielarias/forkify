3382bc30703c37ed7a0ea1c972d3caff3774d53f
jacielarias <jaciel_klax49@hotmail.com>
2024-01-08 14:37:44 -0600

    forkify-final

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '98c12083-f8b5-4ec2-8eed-4f74bba38a0e';
+export const MODAL_CLOSE_SEC = 2.5;
